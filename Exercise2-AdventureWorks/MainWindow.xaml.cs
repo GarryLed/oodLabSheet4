381fa4821846ced232c913a83c28feb84fb71a48
Garry Ledwith <S00256356@atu.ie>
2025-04-18 08:10:54 +0000

    setup and connected to Adventure lite database

--- Exercise2-AdventureWorks/MainWindow.xaml.cs
+++ Exercise2-AdventureWorks/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Exercise2_AdventureWorks
     /// </summary>
     public partial class MainWindow : Window
     {
+        // connecting to database 
         public MainWindow()
         {
             InitializeComponent();
